0be3a1a63cbfe204fe379c786d80ee5a18700880
euprogamoemjavascript <euprogamoemjavascript@outlook.com>
2025-02-19 01:53:51 -0300

    pendente
    pendente

---
+++ pendente.js
@@ -0,0 +1,10 @@
+// Redireciona automaticamente após 10 segundos
+setTimeout(function() {
+    window.location.href = "https://discord.gg/bMwedBgm";
+  }, 10000);
+  
+  // Permite redirecionamento imediato ao clicar no botão
+  document.getElementById("redirectBtn").addEventListener("click", function() {
+    window.location.href = "https://discord.gg/bMwedBgm";
+  });
+  
